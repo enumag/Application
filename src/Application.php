0738f3abc2ff4e364f3862be19a1cd0aa3830d23
Jáchym Toušek <enumag@gmail.com>
2015-06-07 20:59:46 +0200

    Fixed handling of invalid initial request

--- src/Application.php
+++ src/Application.php
@@ -23,13 +23,24 @@ class Application extends BaseApplication
 	/** @var IPresenterFactory */
 	private $presenterFactory;
 
+	/** @var IRouter */
+	private $router;
+
+	/** @var IRequest */
+	private $httpRequest;
+
 	/** @var IResponse */
 	private $httpResponse;
+
+	/** @var Request */
+	private $initialRequest;
 
 	public function __construct(IPresenterFactory $presenterFactory, IRouter $router, IRequest $httpRequest, IResponse $httpResponse)
 	{
 		parent::__construct($presenterFactory, $router, $httpRequest, $httpResponse);
 		$this->presenterFactory = $presenterFactory;
+		$this->router = $router;
+		$this->httpRequest = $httpRequest;
 		$this->httpResponse = $httpResponse;
 	}
 
@@ -69,8 +80,8 @@ class Application extends BaseApplication
 		}
 
 		$requests = $this->getRequests();
-		$request = end($requests);
-		$args = array('exception' => $e, 'request' => $request ?: null);
+		$request = end($requests) ?: $this->initialRequest;
+		$args = array('exception' => $e, 'request' => $request);
 		$errorPresenter = $request ? $this->findErrorPresenter($request->getPresenterName()) : $this->errorPresenter;
 
 		if ($this->presenter instanceof Presenter) {
@@ -82,6 +93,35 @@ class Application extends BaseApplication
 		} else {
 			$this->processRequest(new Request($errorPresenter, Request::FORWARD, $args));
 		}
+	}
+
+	/**
+	 * @return Request
+	 */
+	public function createInitialRequest()
+	{
+		$request = $this->router->match($this->httpRequest);
+
+		if (!$request instanceof Request) {
+			throw new BadRequestException('No route for HTTP request.');
+		}
+
+		$name = ':' . $request->getPresenterName();
+		if (strcasecmp(substr($name, - (strlen($this->errorPresenter) + 1)), ':' . $this->errorPresenter) === 0) {
+			$this->initialRequest = $request;
+			throw new BadRequestException('Invalid request. Presenter is not achievable.');
+		}
+
+		try {
+			$name = $request->getPresenterName();
+			$this->presenterFactory->getPresenterClass($name);
+
+		} catch (InvalidPresenterException $e) {
+			$this->initialRequest = $request;
+			throw new BadRequestException($e->getMessage(), 0, $e);
+		}
+
+		return $request;
 	}
 
 	private function findErrorPresenter($module)
